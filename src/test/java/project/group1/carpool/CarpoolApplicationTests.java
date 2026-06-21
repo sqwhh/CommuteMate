a76055a7fdcd942824b403bc2e59bf01de026d5c
Roman Kalinichenko <sq.whh@icloud.com>
2026-06-21 14:28:58 -0700

    Initial commit

---
+++ src/test/java/project/group1/carpool/CarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package project.group1.carpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
